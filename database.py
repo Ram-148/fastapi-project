83c085ac325c9f7ae932599cf7e8ad6fce78e617
Ram Pratap <ramshyamchoudhary71@gmail.com>
2026-03-25 13:36:25 +0530

    connected neon database

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-db_url = "postgresql://postgres:Rpsp1482006%40@localhost:5432/telusko"
-engine = create_engine(db_url)
+
+db_url = os.getenv("DATABASE_URL")
+
+engine = create_engine(db_url, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# For compatibility with code that expects a callable named `session`
-# (e.g. `from database import session`)
 session = SessionLocal
