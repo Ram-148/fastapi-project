8ec2193988485790515fa99960f1cac02b254c35
Ram Pratap <ramshyamchoudhary71@gmail.com>
2026-03-25 13:55:27 +0530

    fix engine import issue

--- database.py
+++ database.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 
 db_url = os.getenv("DATABASE_URL")
 
+engine = None  # 👈 ADD THIS LINE
+
 if db_url:
     engine = create_engine(db_url, pool_pre_ping=True)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
